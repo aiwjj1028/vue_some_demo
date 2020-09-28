4ad8ef62849a86b84b1d9ecbdc4f4a06684a7973
xx <xxx@ex.com>
2020-09-29 00:24:09 +0800

    vue 打包
    1. 添加静态文件打包路径，使其有上下文时访问不出错
    2. nginx 配置代理，将访问80的xxx/plas/center 转到83端口
    
    Signed-off-by: xx <xxx@ex.com>

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,16 @@
+const path = require('path');
+
+
 module.exports = {
+  assetsDir:'./test/web/',
+  publicPath:'./',
   pages: {
     index: {
       entry: 'src/pages/index/main.js',
       template: 'public/index.html',
       filename: 'index.html',
-      // 当使用 title 选项时，
-      // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
       title: 'Index',
-      // 在这个页面中包含的块，默认情况下会包含
-      // 提取出来的通用 chunk 和 vendor chunk。
-      // chunks: ['chunk-vendors', 'chunk-common', 'index']
     },
-    // 当使用只有入口的字符串格式时，
-    // 模板会被推导为 `public/subpage.html`
-    // 并且如果找不到的话，就回退到 `public/index.html`。
-    // 输出文件名会被推导为 `subpage.html`。
     page1: {
       entry: 'src/pages/page1/main.js',
       template: 'public/page1.html',
@@ -27,6 +23,5 @@ module.exports = {
       filename: 'page2.html',
       title: 'page2'
     }
-
   }
 }
