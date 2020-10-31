427d220af5c94a1a6caaa251c1a462415388efc6
xx <xxx@ex.com>
2020-10-31 19:10:16 +0800

    生命周期

--- src/pages/page1/views/Detail.vue
+++ src/pages/page1/views/Detail.vue
@@ -1,24 +1,41 @@
 <template>
   <div class="detial">
-    this is page1 detial
-    <img src="../../../assets/logo.png"/>
-    <button @click="golist()">进入List页面</button>
+    生命周期
+    <button @click="change">修改</button>
+    <div>{{msg}}</div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'detial',
-  data () {
+  data() {
     return {
-    item:1
+      item: 1,
+      msg: "耀萱"
+    };
+  },
+  methods: {
+    change: function() {
+      this.msg = "狗屁";
     }
   },
-     methods:{
-     golist:function(){
-      this.$router.push({name:'list'})
-     }
+  beforeCreate() {
+    console.log("我是beforeCreate", this.$data);
+  },
+  created() {
+    console.log("我是created", this.$data);
+  },
+  beforeMount() {
+    console.log("我是beforeMount", this.$data);
+  },
+  mounted() {
+    console.log("我是mounted", this.$data);
+  },
+  beforeUpdate() {
+    console.log("我是beforeUpdate", this.$data);
+  },
+  updated() {
+    console.log("我是updated", this.$data);
   }
-}
+};
 </script>
